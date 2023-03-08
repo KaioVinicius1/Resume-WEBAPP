3401e71a22347677f7b8577386ef011bb2c9673e
Kaio Vinicius <kvinicius27play@gmail.com>
2023-03-08 14:47:04 -0300

    atualização

--- assets/js/main.js
+++ assets/js/main.js
@@ -183,3 +183,18 @@
 			});
 
 })(jQuery);
+
+new Swiper('.events-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
